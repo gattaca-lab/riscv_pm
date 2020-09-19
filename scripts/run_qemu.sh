2d676f62f89c67e69eb468ab72da7a829f1178f8
Alex White <space.monkey.delivers@gmail.com>
2020-09-19 13:52:41 +0300

    Use testdev to conduct exit from test

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -2,7 +2,6 @@
 
 ROOT_DIR="${2}"
 
-set -e 
 source ${2}/scripts/env.sh
 
 QEMU_BIN=${INSTALL_DIR}/bin/qemu-system-riscv64
@@ -11,5 +10,7 @@ ${QEMU_BIN} \
     -nographic \
     -machine virt \
     -kernel ${1} \
+    -chardev testdev,id=ctl -serial chardev:ctl \
     -m 1024M
-exit $?
+
+exit $(($?>>1))
